26dd627424c727643289ce93fd05ff4607cea356
Vasiliy <wudgexyz@gmail.com>
2023-09-11 01:13:27 +0300

    added day model

---
+++ src/ConsoleParser/ScheduleModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleParser
+{
+    internal class ScheduleModel
+    {
+
+        public string? DayOfWeek { get; set; }
+        public int LessonNumber { get; set; }
+        public string? LessonType { get; set; }
+        public string? LessonName { get; set; }
+        public string? LessonTeacher { get; set; }
+        public string? LessonRoom { get; set; }
+        public string? StudentGroup { get; set; }
+
+        public int LessonTime { get; set; }
+
+    }
+}
