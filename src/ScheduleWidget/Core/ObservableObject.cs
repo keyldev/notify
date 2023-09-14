b543d473c8eab79dc06de8a70ecee40d0e3a1694
Vasiliy <wudgexyz@gmail.com>
2023-09-14 21:40:07 +0300

    WPF test app added

---
+++ src/ScheduleWidget/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleWidget.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ src/ScheduleWidget/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ScheduleWidget.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleWidget.MVVM.ViewModel
+{
+    internal class MainWindowViewModel : ObservableObject
+    {
+
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set { _currentView = value; NotifyPropertyChanged(); }
+		}
+        public MainWindowViewModel()
+        {
+            CurrentView = new object();
+        }
+
+
+    }
+}
