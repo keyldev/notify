8505b14e062436a371de9ccb0c0724db93ef9223
Vasiliy <wudgexyz@gmail.com>
2023-09-16 18:27:52 +0300

    Lesson card added;

--- src/ScheduleWidget/MVVM/ViewModel/EditDayViewModel.cs
+++ src/ScheduleWidget/MVVM/ViewModel/EditDayViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ScheduleWidget.Core;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,22 @@ namespace ScheduleWidget.MVVM.ViewModel
 {
     class EditDayViewModel : ObservableObject
     {
+		private ObservableCollection<object> _lessonsList;
+
+		public ObservableCollection<object> LessonsList
+		{
+			get { return _lessonsList; }
+			set { _lessonsList = value; NotifyPropertyChanged(); }
+		}
 
 
+        public EditDayViewModel()
+        {
+            LessonsList = new ObservableCollection<object>();
+            LessonsList.Add(new object());
+            LessonsList.Add(new object());
+            LessonsList.Add(new object());
+        }
 
     }
 }
