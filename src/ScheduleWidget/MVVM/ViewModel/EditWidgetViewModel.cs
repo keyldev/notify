8629d0e1a966702ae174681819027a3a71a95ec3
Vasiliy <wudgexyz@gmail.com>
2023-09-16 19:33:07 +0300

    Edit widget settings window; Fonts main;

---
+++ src/ScheduleWidget/MVVM/ViewModel/EditWidgetViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ScheduleWidget.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleWidget.MVVM.ViewModel
+{
+    internal class EditWidgetViewModel : ObservableObject
+    {
+
+
+        public EditWidgetViewModel()
+        {
+            
+        }
+
+    }
+}
